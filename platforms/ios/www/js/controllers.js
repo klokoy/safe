f0cb5e3c09abdd91451ef589fe9f25efc0a6741d
Kim Lokøy <kim.lokoy@gmail.com>
2014-10-24 18:22:25 +0200

    wip

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('LoginCtrl', function($scope, auth, $state, $timeout, $cordovaDeviceOrientation, store) {
+.controller('LoginCtrl', function($scope, auth, $state, $timeout, $ionicModal, $cordovaDeviceOrientation, store) {
     $scope.user = '';
     $scope.pass = '';
 
@@ -53,7 +53,7 @@ angular.module('starter.controllers', [])
 
     function onLoginFailed(error) {
 
-        alert('shit');
+        $scope.openModal();
 
         $scope.model.username = undefined;
     }
@@ -84,9 +84,33 @@ angular.module('starter.controllers', [])
             $scope.model.username = $scope.number;
         }
 
-
-
     };
+
+    $ionicModal.fromTemplateUrl('shit.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+    $scope.openModal = function() {
+        $scope.modal.show();
+    };
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    };
+
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function() {
+        // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+        // Execute action
+    });
 })
 
 
